0edc61545b52ff5937933c58351ab0138bd9b873
EC2 Default User <ec2-user@ip-172-31-29-207.ec2.internal>
2021-05-10 07:11:59 +0000

    [Add]room

--- chatXX/config/routes.rb
+++ chatXX/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#top'
+  root to: 'users#index'
+  resources :users, only:[:show, :edit, :update, :index,]
+  get 'chat/:id' => 'chats#show', as: 'chat'
+  resources :chats, only: [:create]
 end
